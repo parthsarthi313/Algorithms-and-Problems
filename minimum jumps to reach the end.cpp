e0d0495fb6d3342f6dc4d5038c36c3e909e43358
PARTHSARTHI NEEMA <77001772+parthsarthi313@users.noreply.github.com>
2021-07-23 09:47:00 +0530

    Update minimum jumps to reach the end.cpp

--- minimum jumps to reach the end.cpp
+++ minimum jumps to reach the end.cpp
@@ -1,29 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define endl '\n'
-int main()
-{
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i =0;i<n;i++){
-        cin >> arr[i];
-    }
-    int k;
-    cin >> k;
-    int dp[n+1][k+1];
-    for(int i =0;i<=n;i++){
-        for(int j=0;j<=k;j++){
-            if(i == 0 || j == 0) dp[i][j] = 0;
-            else{
-                dp[i][j] = dp[i-1][j];
-                if(arr[i-1] <= j && arr[i-1] > 0){
-                    dp[i][j] += dp[i-1][j/arr[i-1]] + 1;
-                }
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        int low = 0;
+        int high = 0;
+        int jumps = 0;
+        for(int i =0;i<n;i++){
+            high = max(high,arr[i] + i);
+            if(i == low){
+                jumps++;
+                low = high;
             }
+            if(low >=n-1) break;
         }
+        if(low <n-1) return -1;
+        else return jumps;
     }
-    cout << dp[n][k];
-
-}
+};
