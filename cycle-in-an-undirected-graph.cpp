187351fb638ac658738cada9550fc4563fc30dfe
nishant6789 <66275203+nishant6789@users.noreply.github.com>
2021-07-24 11:40:43 +0530

    Add files via upload

--- cycle-in-an-undirected-graph.cpp
+++ cycle-in-an-undirected-graph.cpp
@@ -1,78 +1,43 @@
 /*
 
-Check if the cycle is present in the undirected 
-graph of not.
+Topological Sort snippet
 
 */
 
-#include<bits/stdc++.h>
-#define hell                         1000000007
-#define ll                           long long
-#define endl                           '\n'
 
-using namespace std;
+vector<int>ans;
+int vis[10004];
+void dfs(int node,vector<int>adj[])
+{
+	   vis[node]=1;
+	   for(int child:adj[node])
+	   {
+	       if(vis[child]==0)
+	       {
+	           dfs(child,adj);
+	       }
+	   }
+	   ans.push_back(node);
+}
+vector<int> topoSort(int V, vector<int> adj[]) 
+{
+	    // code here
+	   ans.clear();
+	   int i;
+	   for(i=0;i<V;i++)
+	   {
+	       vis[i]=0;
+	   }
+	    
+	   for(i=0;i<V;i++)
+	   {
+	       if(vis[i]==0)
+	       {
+	           dfs(i,adj);
+	       }
+	   }
+	   if((int)ans.size()>0)
+	   reverse(ans.begin(),ans.end());
+	   return ans;
+}
 
-int vis[100005];
-bool dfs(int node,int par,vector<int> adj[])
-{
-    vis[node]=1;
-    for(int child:adj[node])
-    {
-        if(child==par)
-        continue;
-        if(vis[child]==0)
-        {
-            if(dfs(child,node,adj))
-            return true;
-        }
-        else
-        {
-            return true;
-        }
-    }
-    return false;
-}
-bool isCycle(int V, vector<int>adj[])
-{
-    // Code here
-    int i;
-    for(i=0;i<=V;i++)
-    {
-        vis[i]=0;
-    }
-    for(i=0;i<V;i++)
-    {
-        if(vis[i]==0)
-        {
-            if(dfs(i,-1,adj))
-            return 1;
-        }
-    }
-    return 0;
-}
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int V,E;
-        cin>>V>>E;
-        int i;
-        vector<int>graph[V+1];
-        for(i=0;i<V;i++)
-        {
-            graph[i].clear();
-        }
-        for(i=0;i<E;i++)
-        {
-            int u,v;
-            cin>>u>>v;
-            graph[u].push_back(v);
-        }
-
-        cout<<isCycle(V,graph)<<endl;
-    }
-}
